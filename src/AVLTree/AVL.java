701edb2426f03785f6ce0690332301d139dbd2e0
dhinesh-star <dhineshmanickam2001@gmail.com>
2023-11-24 23:24:35 +0530

    Added comments in the AVL class

--- src/AVLTree/AVL.java
+++ src/AVLTree/AVL.java
@@ -21,6 +21,7 @@ public class AVL {
     }
 
     Node insertIntoBST(Node root1, int val) {
+        //Simple insert as we insert into BST
         if (root1 == null) {
             return new Node(val);
         } else {
@@ -29,22 +30,34 @@ public class AVL {
             } else if (val < root1.data) {
                 root1.left = this.insertIntoBST(root1.left, val);
             }
-
+            //Updating the height of the tree
             root1.height = 1 + Math.max(this.getHeight(root1.left), this.getHeight(root1.right));
+
+            //Calcuating the bstfactor
             int bstFactor = this.getHeight(root1.left) - this.getHeight(root1.right);
+
+            //Convert into balanced bst based of the bstFactor (LL Rotation, LR Rotation, RL Rotation, RR Rotation)
+
+            // LL Rotation
             if (bstFactor > 1 && val < root1.left.data) {
                 return this.rightRotation(root1);
-            } else if (bstFactor > 1 && val > root1.left.data) {
+            }
+            // LR Rotation
+            else if (bstFactor > 1 && val > root1.left.data) {
                 root1.left = this.leftRotation(root1.left);
                 return this.rightRotation(root1);
-            } else if (bstFactor < -1 && val > root1.right.data) {
+            }
+            //RR Rotation
+            else if (bstFactor < -1 && val > root1.right.data) {
                 return this.leftRotation(root1);
-            } else if (bstFactor < -1 && val < root1.right.data) {
+            }
+            //RL Rotation
+            else if (bstFactor < -1 && val < root1.right.data) {
                 root1.right = this.rightRotation(root1.right);
                 return this.leftRotation(root1);
-            } else {
-                return root1;
             }
+            //If the bstFactor is in range[-1,1] then return the node itself
+            return root1;
         }
     }
 
@@ -68,6 +81,7 @@ public class AVL {
         return y;
     }
 }
+//Class to create a tree
 class Node{
     int data,height;
     Node left,right;
